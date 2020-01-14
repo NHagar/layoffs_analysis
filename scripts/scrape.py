ce7e1902feee407ac9de2415607256143bb2c964
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2020-01-14 09:46:03 -0600

    Updates Archive API call, switches to Scrapy scraper

---
+++ scripts/scrape.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import scrapy
+
+df = pd.read_csv('../data/COMPOSITE_29days.csv')
+
+links = df.urls.tolist()
+
+class BFscraper(scrapy.Spider):
+    name = 'buzzfeed'
+
+    custom_settings = {
+        'AUTOTHROTTLE_ENABLED' : True, #make sure set to True for big crawls
+        'HTTPCACHE_ENABLED' : True,
+        'ROBOTSTXT_OBEY' : True,
+        'LOG_LEVEL' : 'INFO'
+        # available levels: CRITICAL, ERROR, WARNING, INFO, DEBUG
+    }
+
+    start_urls = links
+    allowed_domains = "buzzfeednews.com"
+    print("\n\nHello there! Starting with {} urls\n\n".format(len(links)))
+
+    def parse(self, response):
+        #link,hed,subhed,sections,byline,pub_date,html_body,text_body,tags
+
+        link = response.request.url
+        hed = response.css('h1.news-article-header__title').xpath('.//text()').get()
+        subhed = response.css('.news-article-header__dek').xpath('.//text()').get()
+        byline = response.css('span.news-byline-full__name').xpath('.//text()').get()
+        pub_date = response.css('p.news-article-header__timestamps-posted').xpath('.//text()').get()
+
+        text_body = ' '.join(response.css('div.js-article-wrapper').xpath('.//text()').extract())
+        tags = ''.join(response.css('span.tags-list').xpath('.//text()').extract()[1:-1]).replace('#','')
+
+        yield {
+            'link': link,
+            'hed': hed,
+            'subhed': subhed,
+            'byline':byline,
+            'pub_date':pub_date,
+            'text_body':text_body
+        }
