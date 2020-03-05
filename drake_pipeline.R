ba8dafef8a883e44495ff158551937acd2cf243d
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2020-03-04 21:31:26 -0600

    Clean commit before changes

--- drake_pipeline.R
+++ drake_pipeline.R
@@ -1,7 +1,5 @@
 library(drake)
 library(lubridate)
-library(benthos)
-library(cowplot)
 
 set.seed(20200223)
 
@@ -67,6 +65,7 @@ plan <- drake_plan(
     mutate(entity=gsub("\\bthis ", "", entity)) %>% 
     mutate(entity=gsub("'", "", entity)) %>% 
     mutate(entity=gsub("’", "", entity)) %>% 
+    filter(!entity %in% unique(tolower(data_mutate$byline))) %>% 
     distinct(),
   data_entities = entities_filtered %>% 
     left_join(entities) %>% 
@@ -81,113 +80,3 @@ plan <- drake_plan(
 make(plan)
 
 vis_drake_graph(plan)
-
-
-loadd(entities)
-
-loadd(entities_filtered)
-loadd(data_mutate)
-
-entities
-entities_filtered %>% 
-  left_join(entities) %>% 
-  right_join(data_mutate, by=c("doc_id"="link"))
-
-
-entities %>% 
-  filter(grepl("5th", entity))
-
-entities
-
-data("BCI")
-
-
-BCI
-diversity(BCI, index="shannon")
-
-loadd(data_mutate)
-loadd(sections)
-data_mutate
-
-data_mutate %>% 
-  group_by(pub_date, topic) %>% 
-  summarize(stories=n()) %>% 
-  summarize(shannon=shannon(taxon=topic, count=stories)) %>% 
-  deseason(., outcome="shannon", cutoff=as_date("2019-01-25")) %>% 
-  ggplot(aes(relative_days, deseasoned)) + 
-  geom_line() + geom_smooth()
-
-data_mutate %>% 
-  filter(section %in% sections) %>% 
-  group_by(pub_date, section) %>% 
-  summarize(stories=n()) %>% 
-  summarize(shannon=shannon(taxon=section, count=stories)) %>% 
-  deseason(., outcome="shannon", cutoff=as_date("2019-01-25")) %>% 
-  ggplot(aes(relative_days, deseasoned)) + 
-  geom_line() + geom_smooth()
-
-loadd(data_entities)
-data_entities %>% 
-  group_by(entity) %>% 
-  summarize(stories=n()) %>% 
-  filter(stories==1) %>% 
-  left_join(data_entities)
-
-p1 <- data_entities %>% 
-  filter(section=="tech") %>% 
-  group_by(pub_date, entity_type, entity) %>% 
-  summarize(stories=n_distinct(doc_id)) %>% 
-  filter(!is.na(entity)) %>% 
-  summarize(shannon=shannon(taxon=entity, count=stories)) %>%
-  ggplot(aes(pub_date, shannon, color=entity_type)) + 
-  geom_smooth()+ geom_vline(xintercept=as_date("2019-01-25")) + 
-  labs(x="Date", y="Shannon index")
-
-plots <- plot_grid(plot_grid(p1 + theme(legend.position = "none"),
-          p2 + theme(legend.position = "none"), 
-          labels=c("A", "B")),
-          get_legend(p1 +
-                       theme(legend.position = "bottom")), ncol = 1, rel_heights = c(1, .1))
-
-ggsave("C:\\Users\\nrh146\\Dropbox\\Apps\\Overleaf\\buzzfeed-layoffs\\vocabs.png", plots, width = 4, height = 3)
-
-  deseason(., outcome="shannon", cutoff=as_date("2019-01-25")) %>% 
-  ggplot(aes(relative_days, deseasoned, color=entity_type)) + 
-  geom_line() + geom_smooth()
-
-data_mutate %>% 
-  group_by(pub_date) %>% 
-  summarize(stories=n(), authors=n_distinct(byline)) %>% 
-  summarize(cor(stories, authors))
-
-topics <- data_mutate %>% 
-  group_by(pub_date, topic) %>% 
-  summarize(stories=n()) %>% 
-  summarize(shannon=shannon(taxon=topic, count=stories)) %>% 
-  deseason(., outcome="shannon", cutoff=as_date("2019-01-25"))
-  
-model <- RDestimate(deseasoned ~ relative_days, data=topics, cutpoint=0)
-
-plot(model)
-
-  ggplot(aes(relative_days, deseasoned)) + 
-  geom_line() + geom_smooth()
-
-loadd(data_sections)
-data_sections %>% 
-  summarize(shannon=shannon(taxon=section, count=stories)) %>% 
-  ggplot(aes(prepost, shannon)) + 
-  geom_bar(stat='identity')
-
-loadd(data_topics)
-data_topics 
-
-%>% 
-  summarize(shannon=shannon(taxon=topic, count=stories))
-
-read_csv("data/tagged_text_entities.csv") %>% 
-  filter(entity_type %in% c("PERSON", "GPE", "ORG")) %>% 
-  distinct(entity)
-
-data_entities %>% 
-  distinct(entity)
